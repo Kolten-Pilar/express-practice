01686af412c1f8648ad12514dc5422496495baaa
Kolten ✔️ <kolten.pilar@gmail.com>
2023-08-17 11:47:42 -0400

    completed lab

--- server.js
+++ server.js
@@ -32,6 +32,16 @@ app.get('/tip/:total/:tipPercentage', (req, res) => {
   res.send(`<h1>$ ${req.params.total * req.params.tipPercentage / 100}</h1>`)
 })
 
+// magic 8 ball
+const responses = ["It is certain", "It is decidedly so", "Without a doubt", "Yes definitely","You may rely on it", "As I see it yes", "Most likely", "Outlook good","Yes", "Signs point to yes", "Reply hazy try again", "Ask again later","Better not tell you now", "Cannot predict now", "Concentrate and ask again","Don't count on it", "My reply is no", "My sources say no","Outlook not so good", "Very doubtful"]
+
+app.get('/magic/:input', (req, res) => {
+  console.log(req.params);
+  // res.send(responses)
+  // console.log(responses[Math.floor(Math.random() * responses.length)]);
+  res.send(`<h1>${req.params.input}? ${responses[Math.floor(Math.random() * responses.length)]}</h1>`)
+})
+
 
 app.listen(port, () => {
   console.log(`listening on port ${port}`);
